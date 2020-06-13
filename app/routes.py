c39af039ec01e77dea3962bcd7d5b5d5a23a75be
vitorcvargas <56229587+vitorcvargas@users.noreply.github.com>
2020-06-13 12:13:50 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,31 +6,72 @@ from .models import *
 
 @app.route("/")
 def index():
+    
+    balance = Balance.query.first()
 
-    # shows current balance
-    current_balance = Balance.query.first().balance
+    if balance != None:
+        
+        current_balance = balance.balance
+        daily_limit = limit(current_balance)
 
-    daily_limit = limit(current_balance)    
+        return render_template('index.html', balance = balance, 
+        current_balance = "{:.2f}".format(current_balance), 
+        daily_limit = "{:.2f}".format(daily_limit))
 
-    return render_template('index.html', current_balance = "$ {}".format(round(current_balance, 2)), daily_limit = "$ {}".format(round(daily_limit, 2)))
+    else:
+        return render_template('index.html', balance = balance)
 
-@app.route("/add", methods = ["POST"])
-def add():
+@app.route("/update/<int:id>", methods = ["POST"])
+def update(id):
+
+    item = Balance.query.get(id)
+    update = request.form.get('current_balance')
+    item.balance = update
+    db.session.commit()
+
+    return redirect(url_for('index'))        
+
+
+@app.route("/create", methods = ["POST"])
+def create():
+    
+    item = Balance(balance = request.form.get('current_balance'))
+    
+    db.session.add(item)
+    db.session.commit()
+
+    return redirect(url_for('index'))        
+
+
+
+@app.route("/income", methods = ["POST"])
+def income():
 
     income_amount = int(request.form.get('income_amount'))
     income_source = request.form.get('income_source')
 
+    balance = Balance.query.first()
+
+    balance.income(income_amount)
+    income = Income(amount = income_amount, source = income_source)
+    
+    db.session.add(income)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route("/expense", methods = ["POST"])
+def expense():
+
     expense_amount = int(request.form.get('expense_amount'))
     expense_source = request.form.get('expense_source')
 
-    income = Income(amount = income_amount, source = income_source)
+    balance = Balance.query.first()
+
+    balance.expense(expense_amount)
     expense = Expense(amount = expense_amount, source = expense_source)
-    balance = Balance(balance = income_amount - expense_amount)
 
-    db.session.add(income)
-    db.session.add(expense)
-    db.session.add(balance)
-
+    db.session.add(expense)    
     db.session.commit()
 
     return redirect(url_for('index'))
